fb7c7054e9e7bea486f034bf78f28f38ccf4887e
Michael <michael.greub@epfl.ch>
2023-05-08 11:51:51 +0200

    D_10

---
+++ G50/D_10.sql
@@ -0,0 +1,10 @@
+WITH VALID_REVIEWS AS (SELECT Business_Id, COUNT(*) Count FROM Reviews WHERE User_Id IN
+    (SELECT User_Id FROM User_Yelp where User_Id NOT IN (
+        SELECT User_Id FROM (SELECT User_Id, COUNT(Friends_Id) Count FROM Friends GROUP BY USER_ID) WHERE COUNT > 2
+        )
+    ) GROUP BY BUSINESS_ID
+) SELECT NVL(SUM, 0) Review_Count, C.City_Name FROM Cities C LEFT OUTER JOIN
+    (SELECT City_Name, SUM(COUNT) SUM FROM VALID_REVIEWS VR INNER JOIN
+        (SELECT Business_Id, City_Name FROM Business_Location) BL
+        ON BL.Business_Id=VR.Business_Id GROUP BY City_Name) CC
+    ON C.City_Name = CC.City_Name ORDER BY Review_Count, City_Name;
