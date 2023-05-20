64f63a4f026f72e29808b676913aad2f7928cf59
Michael Greub <michael.greub@epfl.ch>
2023-05-20 17:05:18 +0200

    Fix D10 state name

--- G50/D_10.sql
+++ G50/D_10.sql
@@ -4,7 +4,7 @@ WITH VALID_REVIEWS AS (SELECT Business_Id, COUNT(*) Count FROM Reviews WHERE Use
         )
     ) GROUP BY BUSINESS_ID
 ) SELECT NVL(SUM, 0) Review_Count, C.City_Name FROM Cities C LEFT OUTER JOIN
-    (SELECT City_Name, SUM(COUNT) SUM FROM VALID_REVIEWS VR INNER JOIN
-        (SELECT Business_Id, City_Name FROM Business_Location) BL
-        ON BL.Business_Id=VR.Business_Id GROUP BY City_Name) CC
-    ON C.City_Name = CC.City_Name ORDER BY Review_Count, City_Name;
+    (SELECT City_Name, State_Name, SUM(COUNT) SUM FROM VALID_REVIEWS VR INNER JOIN
+        (SELECT Business_Id, City_Name, State_Name FROM Business_Location) BL
+        ON BL.Business_Id=VR.Business_Id GROUP BY City_Name, State_Name) CC
+    ON C.City_Name = CC.City_Name AND C.State_Name = CC.State_Name ORDER BY Review_Count, City_Name;
