0ade5e078522e58f8b67af27d6baab7281ba0d8e
Michael <michael.greub@epfl.ch>
2023-05-23 10:36:10 +0200

    Correct D9

--- G50/D_9.sql
+++ G50/D_9.sql
@@ -1,8 +1,8 @@
 SELECT * FROM (
-    SELECT R.Business_Id, ROW_NUMBER() OVER (PARTITION BY R.Business_Id ORDER BY REVIEW_COUNT DESC) as REVIEWER_RANK, UC.REVIEW_COUNT FROM (
+    SELECT R.Business_Id, ROW_NUMBER() OVER (PARTITION BY R.Business_Id ORDER BY Review_Count DESC) as Reviewer_Rank, UC.Review_Count FROM (
         ((SELECT Business_Id FROM Business ORDER BY Review_Count DESC FETCH FIRST 10 ROWS ONLY) B
-            INNER JOIN REVIEWS R ON B.Business_Id = R.Business_Id)
+            INNER JOIN Reviews R ON B.Business_Id = R.Business_Id)
         INNER JOIN
-            (SELECT USER_ID, COUNT(*) AS REVIEW_COUNT FROM Reviews GROUP BY USER_ID ORDER BY COUNT(*) DESC) UC
+            (SELECT User_Id, COUNT(*) AS Review_Count FROM Reviews GROUP BY User_Id ORDER BY COUNT(*) DESC) UC
         ON R.User_Id = UC.User_Id))
-WHERE REVIEWER_RANK < 4 ORDER BY Business_Id, REVIEWER_RANK;
+WHERE Reviewer_Rank < 4 ORDER BY Business_Id, Reviewer_Rank;
