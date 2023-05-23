e0cd0d34c56be50edea4c779ad76ef02b8804db5
Michael <michael.greub@epfl.ch>
2023-05-23 11:04:49 +0200

    Fix D2

--- G50/D_2.sql
+++ G50/D_2.sql
@@ -1,2 +1,2 @@
-SELECT State_Name, COUNT(*) AS Num_Businesses FROM Business_Location
+SELECT State_Name, COUNT(DISTINCT Business_Id) AS Num_Businesses FROM Business_Location
     GROUP BY State_Name ORDER BY Num_Businesses DESC FETCH FIRST 10 ROWS ONLY;
