b7c02b95196365744236319048b72d174f7c75fa
Michael <michael.greub@epfl.ch>
2023-06-01 17:14:15 +0200

    Few fixes for mostly easy ones

--- G50/E_6.sql
+++ G50/E_6.sql
@@ -1,11 +1,3 @@
-SELECT USER_ID, COUNT(DISTINCT BUSINESS_ID) FROM REVIEWS
-GROUP BY USER_ID
-ORDER BY COUNT(BUSINESS_ID) DESC
-FETCH FIRST 1 ROW ONLY;
-
--- TODO If I understood correctly, shouldn't we only return the count column ?
--- If that's the case I propose :) :
-
 SELECT MAX(numReviews) AS count
 FROM (
     SELECT R.USER_ID, COUNT(DISTINCT R.BUSINESS_ID) AS numReviews
